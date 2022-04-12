cc8ec2d6c6e4299f5ef23f48f59efdf5558674d0
Cristhian <cristhianclx@gmail.com>
2022-04-11 19:07:50 -0500

    update terraform

--- talleres/02/terraform/remote.tf
+++ talleres/02/terraform/remote.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket                  = "infrastructure.demo.pe"
-    key                     = "tf/main/sre.tfstate"
-    encrypt                 = "true"
-    region                  = "sa-east-1"
+    bucket  = "infrastructure.demo.pe"
+    key     = "tf/main/sre.tfstate"
+    encrypt = "true"
+    region  = "sa-east-1"
   }
 }
